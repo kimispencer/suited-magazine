e21b01682d04d42d4aff5f5d73c74d19b839d6b1
kimi spencer <kimispencer@gmail.com>
2016-09-17 01:39:47 -0400

    responsive nav/header

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,8 @@
-// $('.close-nav').on('click', function() {
-	console.log('hello')
-// });
+$('.open-nav').on('click', function() {
+	$('.nav-header').addClass('open');
+	console.log('open nav')
+});
+$('.close-nav').on('click', function() {
+	$('.nav-header').removeClass('open');
+	console.log('close nav')
+});
