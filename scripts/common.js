03e843788b8d6af6dcbe4d09fb0bc28c7a18b293
BrainchildCo <brainchildco@unos-MBP.home>
2016-09-20 00:15:18 -0400

    sesame

--- scripts/common.js
+++ scripts/common.js
@@ -11,12 +11,12 @@ var modalData = {
 			season: 'FALL/WINTER 2017',
 			copy: 'Beauty may be the ultimate enigma: infinite, immeasurable and timeless. Often found in the darker reaches of pain and suffering, it is in life and death, in struggle and triumph. Beauty\'s one constant is its sphinx-like inscrutability; no two people will see it the same way or find it in the same place. Beauty may be in the eye of the beholder, but it is also something held deep within ourselves. Yet the fact that beauty cannot be defined has not stopped an industry from trying, not only in categorizing our differences but homogenizing them, too. Our cover story, photographed by Nicholas Alan Cope, highlights Patricia Black, a woman who is unapologetically herself in embracing these differences. A curator and “stylist to the stylists,” she is in many ways the consummate insider, yet she remembers the feeling of being on the outside looking in, of wanting, as she puts it, to “belong in the room.” She has encouraged a much-needed realness in the industry, her dynamic energy a testament to a life lived true to herself. With this issue, we join Black in defining beauty on our own terms.',
 			extra: ' Beloved author, activist, therapist and muse to photographers like Robert Mapplethorpe and Peter Hujar, Robert Levithan exemplified the idea that beauty can be found in the truth, and in living a life true to oneself. Photographed by Zoltan Tombor. *With Robert\'s recent passing on May 13, 2016, we\'re honored to present this story in his loving memory. Six industry insiders—Linda Rodin, Poppy King, Chris Salgardo, Michelle Harper, Catherine Walsh and Linda Wells—challenge and reflect on what it means to be beautiful. Portraits by David Slijper.'
-		},
+		}
 	],
 	contact: [
 		{
 			title: 'Contact Us',
-			address: '140 Hope St Brooklyn NY'
+			address: '666 Sesame St Brooklyn NY'
 		}
 	]
 }
