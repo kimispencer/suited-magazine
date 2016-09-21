f689a02e74d04802de47dcbd14f42e896a734d4b
kimi spencer <kimispencer@gmail.com>
2016-09-21 16:05:44 -0400

    purchase link-borders

--- scripts/common.js
+++ scripts/common.js
@@ -69,7 +69,7 @@ $('.open-modal').on('click', function() {
 								+ '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">'
 									+ '<input type="hidden" name="cmd" value="_s-xclick">'
 									+ '<input type="hidden" name="hosted_button_id" value="WK4V3PRHWPHAC">'
-									+ '<div class="link-border">'
+									+ '<div class="link-border title">'
 										+ '<input type="submit" class="submit" name="submit" value="Purchase U.S.">'
 									+ '</div>'
 								+ '</form>'
